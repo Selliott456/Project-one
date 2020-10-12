acc691f438fd2fdecbcb02c5b7cd2adebc5b2743
Sherryll Elliott <sherryllelliott@gmail.com>
2020-10-12 16:36:15 +0100

    bullet stops after first row

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ for (let i = 0; i < width ** 2; i++) {
 cells[player].classList.add('player')
 // ************************************
 
-function printAliensOne() {
+function printAliensLeft() {
   for (let rows = 1; rows < 4; rows++)
     for (let i = 0; i < rows * width; i++) {
       if (i === width - 1 || i === 2 * width - 1 || i === 3 * width - 1) {
@@ -31,34 +31,47 @@ function printAliensOne() {
     }
 }
 
-printAliensOne()
+printAliensLeft()
 
 // ******************************************
-{
-  document.addEventListener('keypress', (event) => {
 
-    const key = event.key
-    if (key === "a" && player > 72) {
-      console.log('working')
-      cells[player].classList.remove('player')
-      player -= 1
-      cells[player].classList.add('player')
-    } else if (key === "d" && player < 80) {
-      console.log('working')
-      cells[player].classList.remove('player')
-      player += 1
-      cells[player].classList.add('player')
-    } else if (key === 's') {
-      for (let i = 0; i < width; i++) {
-        let bullet = player
-        const interval = setInterval(() => {
-          cells[bullet].classList.remove('bullet')
-          bullet -= width
-          cells[bullet].classList.add('bullet')
+document.addEventListener('keypress', (event) => {
 
-        }, 100)
+  const key = event.key
+  if (key === "a" && player > 72) {
+    console.log('working')
+    cells[player].classList.remove('player')
+    player -= 1
+    cells[player].classList.add('player')
+  } else if (key === "d" && player < 80) {
+    console.log('working')
+    cells[player].classList.remove('player')
+    player += 1
+    cells[player].classList.add('player')
+  } else if (key === 's') {
+    shoot()
+    killAlien()
+  }
+})
+
+function shoot() {
+  let bullet = player
+
+  for (let i = 0; i < 8; i++) {
+    let bullet = player
+    const interval = setInterval(() => {
+      if (bullet > 0) {
+        cells[bullet].classList.remove('bullet')
+        bullet -= width
+        cells[bullet].classList.add('bullet')
+      } else if (bullet < 0) {
+        clearInterval(interval)
       }
+    }, 200)
+  }
+}
 
-    }
-  })
-} 
+function killAlien(){
+  console.log(cells.innerHTML)
+
+}
